7cc53eb3a82b5e21e8bf54e6d3ea031353d9c82c
mohajon33 <92334273+mohajon33@users.noreply.github.com>
2021-10-19 10:22:07 +0600

    Adding Dynamic Styles with wrap up

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import ExpensesList from "./ExpensesList";
 import "./Expense.css";
+import ExpenseChart from "./ExpenseChart";
 
 function Expense(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -23,6 +24,7 @@ function Expense(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpenseChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
 
       {/* <ExpenseItem
